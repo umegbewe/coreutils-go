5bb787c3bfb6e4ce418c93acfdb585a6c82191a7
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2023-05-22 02:27:48 -0500

    handle file and direcory permissions, and add totalSize of directories

--- ls/ls.go
+++ ls/ls.go
@@ -31,6 +31,74 @@ func quote(s string) string {
 	return s
 }
 
+func formatPermissions(mode os.FileMode) string {
+	var b strings.Builder
+
+	b.WriteString(fileType(mode))
+	b.WriteString(filePermissions(mode))
+
+	if mode&os.ModeSticky != 0 {
+		b.WriteRune('t')
+	} else if mode&os.ModeSetuid != 0 {
+		b.WriteRune('s')
+	} else if mode&os.ModeSetgid != 0 {
+		b.WriteRune('s')
+	} else {
+		b.WriteRune('-')
+	}
+
+	return b.String()
+}
+
+func fileType(mode os.FileMode) string {
+	switch {
+	case mode.IsRegular():
+		return "-"
+	case mode.IsDir():
+		return "d"
+	case mode&os.ModeSymlink != 0:
+		return "l"
+	case mode&os.ModeNamedPipe != 0:
+		return "p"
+	case mode&os.ModeSocket != 0:
+		return "s"
+	case mode&os.ModeDevice != 0:
+		if mode&os.ModeCharDevice != 0 {
+			return "c"
+		}
+		return "b"
+	default:
+		return "?"
+	}
+}
+
+func filePermissions(mode os.FileMode) string {
+	var perms [9]rune
+
+	fillPerms(perms[:], 0, mode.Perm()>>6)
+	fillPerms(perms[:], 3, (mode.Perm()>>3)&7)
+	fillPerms(perms[:], 6, mode.Perm()&7)
+	return string(perms[:])
+}
+
+func fillPerms(perms []rune, start int, p os.FileMode) {
+	if p&4 != 0 {
+		perms[start] = 'r'
+	} else {
+		perms[start] = '-'
+	}
+	if p&2 != 0 {
+		perms[start+1] = 'w'
+	} else {
+		perms[start+1] = '-'
+	}
+	if p&1 != 0 {
+		perms[start+2] = 'x'
+	} else {
+		perms[start+2] = '-'
+	}
+}
+
 func formatSize(size int64) string {
 	if *hFlag {
 		const unit = 1024
@@ -74,7 +142,7 @@ func formatFile(file os.FileInfo, path string) string {
 		}
 		return fmt.Sprintf(
 			"%v %d %s %s %s %s %s%s",
-			file.Mode(),
+			formatPermissions(file.Mode()),
 			file.Sys().(*syscall.Stat_t).Nlink,
 			userName,
 			groupName,
@@ -100,6 +168,15 @@ func ls(dirname string, recursive bool) {
 	if err != nil {
 		fmt.Println(err)
 		return
+	}
+
+	totalSize := int64(0)
+
+	if *lFlag {
+		for _, file := range files {
+			totalSize += file.Sys().(*syscall.Stat_t).Blocks
+		}
+		fmt.Printf("total %d\n", totalSize)
 	}
 
 	if *dFlag {
